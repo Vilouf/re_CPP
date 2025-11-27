ba0c965f85fca97b08b1c6351c14853af7e17011
Victor Elbling <vielblin@k1r4p12.42mulhouse.fr>
2025-11-27 16:20:20 +0100

    mandatory cpp02 done

---
+++ cpp02/ex03/Point.hpp
@@ -0,0 +1,23 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+
+#include "Fixed.hpp"
+
+class Point {
+
+private:
+
+	const Fixed	_x;
+	const Fixed _y;
+
+public:
+
+	Point();
+	Point( const float x, const float y );
+	Point( const Point & );
+	Point	&operator=( const Fixed &);
+	~Point();
+
+};
+
+#endif
